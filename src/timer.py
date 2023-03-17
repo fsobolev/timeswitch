a6348fe6d65b869c9d8fb387bf1f95ade7f116c4
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-03-17 19:10:52 +0300

    Add message in backgound apps UI
    Closes #36

--- src/timer.py
+++ src/timer.py
@@ -28,7 +28,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-from gi.repository import GObject
+from gi.repository import GObject, GLib, Gio
 from .actions import *
 
 
@@ -38,8 +38,10 @@ class Timer:
         self.duration = self.h * 3600 + self.m * 60 + self.s
         self.action = action[0]
         self.desc_label = desc_label
+        self.status_label = ""
         self.pause_button = pause_button
         self.pause = False
+        self.bus = Gio.bus_get_sync(Gio.BusType.SESSION, None)
 
         # Notification
         self.sound_repeat = False
@@ -47,12 +49,16 @@ class Timer:
 
         if self.action == 'poweroff':
             self.desc_label.set_text(_('Your device will be powered off in'))
+            self.status_label = _('Power off in {}')
         elif self.action == 'reboot':
             self.desc_label.set_text(_('Your device will be rebooted in'))
+            self.status_label = _('Reboot in {}')
         elif self.action == 'suspend':
             self.desc_label.set_text(_('Your device will be suspended in'))
+            self.status_label = _('Suspend in {}')
         elif self.action == 'notification':
             self.desc_label.set_text(_('You will receive a notification in'))
+            self.status_label = _('Notification in {}')
             self.notification_text = action[1]
             self.play_sound = action[2]
             if action[2] == 2:
@@ -60,6 +66,7 @@ class Timer:
         elif self.action == 'command':
             self.desc_label.set_markup(_(
                 'The command <b>{}</b> will be executed in').format(action[1]))
+            self.status_label = _('Command "{}" in {}').format(action[1], '{}')
             self.cmd = action[2]
         self.timer_label = timer_label
         self.finish_fn = finish_fn
@@ -68,16 +75,31 @@ class Timer:
 
     def run(self):
         if self.pause: return True
-        if self.stop: return False
+        if self.stop:
+            self.clear_status()
+            return False
         self.s = self.duration
         self.h = self.s // 3600
         self.s %= 3600
         self.m = self.s // 60
         self.s %= 60
-        self.timer_label.set_text(
-            str(self.h) +
-            ':{0:0>2}'.format(self.m) +
-            ':{0:0>2}'.format(self.s))
+        time_label = str(self.h) + \
+            ':{0:0>2}'.format(self.m) + \
+            ':{0:0>2}'.format(self.s)
+        self.timer_label.set_text(time_label)
+        self.bus.call('org.freedesktop.portal.Desktop', # Bus name
+            '/org/freedesktop/portal/desktop', # Object path
+            'org.freedesktop.portal.Background', # Interface name
+            'SetStatus', # Method name
+            GLib.Variant.new_tuple(GLib.Variant('a{sv}', \
+                {'message': GLib.Variant('s', \
+                self.status_label.format(time_label))} )), # Parameters
+            None, # Reply type
+            Gio.DBusCallFlags.NONE, # Flags
+            -1, # Timeout
+            None, # Cancellable
+            None, # Callback
+            None) # User data
         if self.duration > 0:
             self.duration -= 1
             return True
@@ -89,6 +111,7 @@ class Timer:
                     _('The notification has been sent.'))
             if not self.sound_repeat:
                 self.finish_fn()
+            self.clear_status()
             return False
 
     def act(self):
@@ -112,3 +135,17 @@ class Timer:
         self.stop = True
         self.player_cancellable.cancel()
 
+    def clear_status(self):
+        self.bus.call('org.freedesktop.portal.Desktop', # Bus name
+            '/org/freedesktop/portal/desktop', # Object path
+            'org.freedesktop.portal.Background', # Interface name
+            'SetStatus', # Method name
+            GLib.Variant.new_tuple(GLib.Variant('a{sv}', \
+                {'message': GLib.Variant('s', \
+                _('Timer has finished!'))} )), # Parameters
+            None, # Reply type
+            Gio.DBusCallFlags.NONE, # Flags
+            -1, # Timeout
+            None, # Cancellable
+            None, # Callback
+            None) # User data'
